cddc0f0c21d0bcd30cc8069c30ddc52e9d94907f
Mert Torun <mert@merttorun.com>
2022-06-28 21:23:21 +0300

    <Menu> shows only the button when not open

--- src/components/Menu.cy.jsx
+++ src/components/Menu.cy.jsx
@@ -1,5 +1,16 @@
+import Menu from "./Menu";
+
 describe("<Menu>", function() {
-    it("shows only the button when not open");
+    it("shows only the button when not open", function() {
+        cy.mount(
+            <Menu title="Closed">
+            <Menu.Item>Item 1</Menu.Item>
+            <Menu.Item>Item 2</Menu.Item>
+            </Menu>
+        );
+        cy.get("button").contains("Closed");
+        cy.get("li").should("not.exist");
+    });
     it("shows the <Menu.Item>s when open");
     it("emits onSelect event with the item ID when an item is clicked");
     it("closes when selected");
---
+++ src/components/Menu.jsx
@@ -0,0 +1,5 @@
+import Button from "./Button";
+
+export default function Menu({ title }) {
+    return <Button icon="down-arrow">{title}</Button>;
+}

0e4d15fe543f310aa609598f755ce9ab74799132
Mert Torun <mert@merttorun.com>
2022-06-28 18:37:22 +0300

    <Button> picks its base colors

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -5,7 +5,15 @@ describe("<Button>", () => {
         cy.mount(<Button>Hello</Button>);
         cy.get("button").contains("Hello");
     });
-    it("picks its theme color");
+    it("picks its base colors", function() {
+        cy.mount(<Button>Base</Button>);
+        cy.get("button")
+            .css()
+            .should("include", { 
+                "background-color": "rgb(27, 197, 189)",
+                "color": "rgb(255, 255, 255)"
+            });
+    });
     it("renders its icon");
     it("emits a click event");
 });

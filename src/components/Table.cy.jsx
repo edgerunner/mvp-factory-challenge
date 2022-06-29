b4531ee983a348c0635c5a35f24cf14df70ec36e
Mert Torun <mert@merttorun.com>
2022-06-29 16:09:11 +0300

    Scaffold tests for <Table>

---
+++ src/components/Table.cy.jsx
@@ -0,0 +1,5 @@
+describe("<Table>", function() {
+    it("renders data as specified by <Column>s");
+    it("renders column headers as specified by <Column>s");
+    it("renders a given caption");
+});

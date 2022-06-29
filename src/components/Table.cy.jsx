45eef241bcb388f2ccf22d8cf1234005d0e598e7
Mert Torun <mert@merttorun.com>
2022-06-29 16:36:28 +0300

    <Table> renders data as specified by <Column>s

--- src/components/Table.cy.jsx
+++ src/components/Table.cy.jsx
@@ -1,5 +1,22 @@
+import Table, { Column } from "./Table";
+
+const sample = [
+    { id: 1, name: "John", age: 20 },
+    { id: 2, name: "Jane", age: 21 },
+    { id: 3, name: "Joe", age: 22 },
+];
 describe("<Table>", function() {
-    it("renders data as specified by <Column>s");
+    it("renders data as specified by <Column>s", function() {
+        cy.mount(
+            <Table data={sample}>
+                <Column>{row => row.id}</Column>
+                <Column>{row => row.name}</Column>
+                <Column>{row => row.age}</Column>
+            </Table>
+        );
+        cy.get("table tr:nth-child(1) td:nth-child(2)").contains("John");
+        cy.get("table tr:nth-child(3) td:nth-child(3)").contains("22");
+    });
     it("renders column headers as specified by <Column>s");
     it("renders a given caption");
 });
---
+++ src/components/Table.jsx
@@ -0,0 +1,30 @@
+import { Children, useMemo } from "react";
+
+export default function Table({ children: columns, data }) {
+    const schema = useMemo(() => makeSchema(columns), [columns]);
+
+    return (
+        <table>
+            <tbody>
+                {data.map((row, index) => <Row schema={schema} data={row} key={row.key || row.id || index} />)}
+            </tbody>
+        </table>
+    );
+}
+
+export function Column() { return null; }
+
+function Row({ schema, data }) {
+    return (
+        <tr>
+            {schema.map(({ render, id }, index) => 
+                <td key={id || index}>{render(data)}</td>)}
+        </tr>
+    );
+}
+
+function makeSchema(columns) {
+    return Children.map(columns, 
+        ({ props: { children: render, id } }) => ({ render, id })
+    );
+}

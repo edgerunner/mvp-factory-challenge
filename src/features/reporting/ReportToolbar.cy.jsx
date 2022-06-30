5d1377e92966ac96b9f60268938711a0f76fb045
Mert Torun <mert@merttorun.com>
2022-07-01 01:38:59 +0300

    Scaffold tests for <ReportToolbar>

---
+++ src/features/reporting/ReportToolbar.cy.jsx
@@ -0,0 +1,7 @@
+describe("<ReportToolbar>", function() {
+    it("renders a project selector");
+    it("renders a gateway selector");
+    it("renders a from-to date selectors");
+    it("renders a generate report button");
+    it("emits an onSubmit event when the generate report button is clicked");
+});

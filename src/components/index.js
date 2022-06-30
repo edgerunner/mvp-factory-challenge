02f1f79d54ec1d736a4f92023025eceb195fcc13
Mert Torun <mert@merttorun.com>
2022-07-01 02:05:05 +0300

    Export components at the folder level

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Button";
+export { default as Currency } from "./Currency";
+export { default as DataBlock } from "./DataBlock";
+export { default as DataSection } from "./DataSection";
+export { default as Date } from "./Date";
+export { default as Menu } from "./Menu";
+export { default as Table } from "./Table";

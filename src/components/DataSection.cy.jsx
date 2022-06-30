9311991852d5032ce5687df8bfe2f9bb3e70732d
Mert Torun <mert@merttorun.com>
2022-06-30 14:02:39 +0300

    Scaffold tests for <DataSection>

---
+++ src/components/DataSection.cy.jsx
@@ -0,0 +1,5 @@
+describe("<DataSection>", function() {
+    it("generates <DataBlock>s from its data");
+    it("generates <DataBlock> headers with a render prop");
+    it("renders a header block");
+});

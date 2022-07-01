7cc131f25b6545d5078fe0adebbb10d3de7364e8
Mert Torun <mert@merttorun.com>
2022-07-01 12:03:03 +0300

    <Summary> just wraps its contents

---
+++ src/components/Summary.jsx
@@ -0,0 +1,8 @@
+import "./Summary.css";
+export default function Summary({ children }) {
+    return (
+        <footer className="Summary">
+            {children}
+        </footer>
+    );    
+}

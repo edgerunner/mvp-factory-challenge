bedd63e3f2f25581e44a009a443944d471a3c620
Mert Torun <mert@merttorun.com>
2022-07-03 23:01:36 +0300

    Scaffold tests for <Report>
    The responsibility for rendering different report types depends on <Report> which ignores the actual request, and determines the result based solely on the returned data.

---
+++ src/features/reporting/Report.cy.jsx
@@ -0,0 +1,6 @@
+describe("<Report>", function() {
+    it("renders the the given data");
+    it("consolidates data for a single project");
+    it("consolidates data for a single gateway");
+    it("consolidates data for single project and gateway");
+});

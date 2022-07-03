4892736d2fb2bc2d1372b0157cb7a324253789ce
Mert Torun <mert@merttorun.com>
2022-07-03 03:46:01 +0300

    Add fixtures and stub out routes for component testing

--- src/features/reporting/Reports.cy.jsx
+++ src/features/reporting/Reports.cy.jsx
@@ -4,23 +4,21 @@ describe("<Reports>", {
     viewportWidth: 1280,
     viewportHeight: 720,
 }, function() {
-    it("renders placeholder pane on mount", function() {
+    beforeEach(function() {
+        cy.intercept("GET", "**/projects", { fixture: "projects" }).as("projects");
+        cy.intercept("GET", "**/gateways", { fixture: "gateways" }).as("gateways");
         cy.mount(<Reports />);
+    });
+    it("renders placeholder pane on mount", function() {
         cy.get("#reports-placeholder h1").contains("No reports");
     });
     
     describe("projects and gateways", function() {
         it("requests the list of projects and gateways on mount", function() {
-            cy.intercept("GET", "**/projects").as("projects");
-            cy.intercept("GET", "**/gateways").as("gateways");
-            cy.mount(<Reports />);
             cy.wait("@projects");
             cy.wait("@gateways");
         });
         it("renders the reports toolbar when the request succeeds", function() {
-            cy.intercept("GET", "**/projects").as("projects");
-            cy.intercept("GET", "**/gateways").as("gateways");
-            cy.mount(<Reports />);
             cy.wait("@projects");
             cy.wait("@gateways");
             cy.get("#report-toolbar");

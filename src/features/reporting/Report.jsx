20867244116e3371fad6dc748ddcabbc3d802e08
Mert Torun <mert@merttorun.com>
2022-07-05 02:22:19 +0300

    Refactor and memoize <Report> transformation
    Now a single pass through the report entries is enough to transform it to the needed format.

--- src/features/reporting/Report.jsx
+++ src/features/reporting/Report.jsx
@@ -1,33 +1,17 @@
+import { useMemo } from "react";
 import { DataSection, Date as Date_, Currency, Column } from "/src/components";
 
-export default function Report({ report, projects, gateways }) {
-    projects = projects.reduce((map, project) =>
-        map.set(project.projectId, project), new Map());
-    gateways = gateways.reduce((map, gateway) =>
-        map.set(gateway.gatewayId, gateway), new Map());
-
-    const resolved = resolve(report, projects, gateways);
-    const [projectId, gatewayId] = extract(resolved);
-    const projectName = projectId ? resolved[0].project.name : "All projects";
-    const gatewayName = gatewayId ? resolved[0].gateway.name : "All gateways";
-    
-    const partitionBy = projectId && !gatewayId ? "gateway" : "project";
-    const partitioned = partition(resolved, partitionBy, {projects, gateways});
-
-    const blockHeader = block => projectId && gatewayId
-        ? null
-        : block[0][partitionBy].name;
+export default function Report(props) {
+    const { mode, data, names, renderHeader } = useTransform(props);
 
     return (
         <div id="report">
             <DataSection 
-                data={partitioned}
-                header={`${projectName} | ${gatewayName}`}
-                blockHeader={blockHeader}>
+                data={data}
+                header={`${names.project} | ${names.gateway}`}
+                blockHeader={renderHeader}>
                 <Column header="Date">{row => <Date_ date={row.created}/>}</Column>
-                {partitionBy === "project" && !gatewayId
-                    ? <Column header="Gateway">{row => row.gateway.name}</Column>
-                    : null}
+                {mode === "all" && <Column header="Gateway">{row => row.gateway.name}</Column>}
                 <Column header="Amount">
                     {row => <Currency amount={row.amount} code="USD"/>}
                 </Column>
@@ -36,33 +20,63 @@ export default function Report({ report, projects, gateways }) {
     );
 }   
 
-function resolve(rawData, projects, gateways) {
-    return rawData.map(block => ({
-        ...block,
-        project: projects.get(block.projectId),
-        gateway: gateways.get(block.gatewayId),
-        created: new Date(block.created),
-        modified: new Date(block.modified)
-    }));
-}
+function useTransform({ report, projects, gateways }) {
+    return useMemo(() => {
+        projects = projects.reduce((map, project) =>
+            map.set(project.projectId, { ...project, blocks: [] }),
+        new Map());
+        gateways = gateways.reduce((map, gateway) =>
+            map.set(gateway.gatewayId, { ...gateway, blocks: [] }),
+        new Map());
 
-function partition(data, partitionBy, entities) {
-    return [...data.reduce((map, block) =>
-        map.set(block[partitionBy],
-            [...(map.get(block[partitionBy]) || []), block])
-    , new Map(
-        Array.from(entities[`${partitionBy}s`].values())
-            .map(entity => [entity, []])
-    )).values()
-    ];
-}
+        for (const block of report) {
+            block.project = projects.get(block.projectId);
+            block.gateway = gateways.get(block.gatewayId);
+            block.created = new Date(block.created);
+            block.modified = new Date(block.modified);
 
-function extract(data) {
-    let projectId = data[0].projectId;
-    let gatewayId = data[0].gatewayId;
-    for (const block of data) {
-        if (block.projectId !== projectId) projectId = false;
-        if (block.gatewayId !== gatewayId) gatewayId = false;
-    }
-    return [ projectId, gatewayId ];
+            block.project.blocks.push(block);
+            block.gateway.blocks.push(block);
+        }
+
+        for (const project of projects.values())
+            if (!project.blocks.length) 
+                projects.delete(project.projectId);
+        for (const gateway of gateways.values())
+            if (!gateway.blocks.length)
+                gateways.delete(gateway.gatewayId);
+
+        const mode =
+            projects.size > 1 && gateways.size > 1 && "all" ||
+            projects.size === 1 && gateways.size > 1 && "project" ||
+            projects.size > 1 && gateways.size === 1 && "gateway" ||
+            projects.size === 1 && gateways.size === 1 && "single";
+
+        const data = {
+            all: [...projects.values()].map(project => project.blocks),
+            project: [...gateways.values()].map(gateway => gateway.blocks),
+            gateway: [...projects.values()].map(project => project.blocks),
+            single: [projects.values().next().value.blocks]
+        }[mode];
+
+        const names = {
+            all: { project: "All projects", gateway: "All gateways" },
+            project: { project: projects.values().next().value.name, gateway: "All gateways" },
+            gateway: { project: "All projects", gateway: gateways.values().next().value.name },
+            single: { 
+                project: projects.values().next().value.name,
+                gateway: gateways.values().next().value.name
+            }
+        }[mode];
+
+        const renderHeader = {
+            all: rows => rows.values().next().value.project.name,
+            project: rows => rows.values().next().value.gateway.name,
+            gateway: rows => rows.values().next().value.project.name,
+            single: () => null
+        }[mode];
+
+        return { mode, data, names, renderHeader };
+
+    }, [report, projects, gateways]);
 }

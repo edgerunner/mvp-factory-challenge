dfafedaf171d68129e8e26f241d343d1977d061c
Mert Torun <mert@merttorun.com>
2022-07-06 13:03:40 +0300

    <PercentagePieChart> renders the given data as percentages

---
+++ src/features/reporting/PercentagePieChart.cy.jsx
@@ -0,0 +1,21 @@
+import PercentagePieChart from "./PercentagePieChart.jsx";
+
+const sample = [
+    { name: "Project 1", total: 3750 },
+    { name: "Project 2", total: 500 },
+    { name: "Project 3", total: 7500 },
+];
+
+describe("<PercentagePieChart>", function() {
+    it("renders the given data as percentages", function() {
+        cy.mount(<PercentagePieChart data={sample}/>);
+
+        cy.contains("text", "32%").should("have.attr", "name", "Project 1");
+        cy.contains("text", "4%").should("have.attr", "name", "Project 2");
+        cy.contains("text", "64%").should("have.attr", "name", "Project 3");
+
+        cy.contains("ul.legend li", "Project 1");
+        cy.contains("ul.legend li", "Project 2");
+        cy.contains("ul.legend li", "Project 3");
+    });
+});

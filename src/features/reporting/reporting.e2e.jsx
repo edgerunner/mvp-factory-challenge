61f804c821c1e605919518e13afdb9738a077482
Mert Torun <mert@merttorun.com>
2022-07-06 14:11:58 +0300

    E2E: Reporting can list and chart transactions for one entity
    Entity: either a project or a gateway

--- src/features/reporting/reporting.e2e.jsx
+++ src/features/reporting/reporting.e2e.jsx
@@ -15,4 +15,35 @@ describe("Reporting", {
 
         cy.contains("footer", /total/i);
     });
+
+    it("can list transactions for a single project", function() {
+        cy.visit("/");
+
+        cy.contains("button", /projects/i).click().get("menu li:last-child").click();
+        cy.contains("button.action", "report").click();
+
+        cy.contains("header", "All projects").should("not.exist");
+        cy.contains("header", "All gateways");
+
+        cy.get("table").should("exist");
+        cy.get("div.chart").should("exist");
+
+        cy.contains("footer", /total/i);
+
+    });
+
+    it("can list transactions for a single gateway", function() {
+        cy.visit("/");
+
+        cy.contains("button", /gateways/i).click().get("menu li:last-child").click();
+        cy.contains("button.action", "report").click();
+
+        cy.contains("header", "All projects");
+        cy.contains("header", "All gateways").should("not.exist");
+
+        cy.get("table").should("exist");
+        cy.get("div.chart").should("exist");
+
+        cy.contains("footer", /total/i);
+    });
 });

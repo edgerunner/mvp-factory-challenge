a7bf1c711bcd8d12785ddf359fc82cb726d0df77
Mert Torun <mert@merttorun.com>
2022-07-06 14:13:53 +0300

    E2E: Reporting can list transactions for a single project and gateway

--- src/features/reporting/reporting.e2e.jsx
+++ src/features/reporting/reporting.e2e.jsx
@@ -46,4 +46,20 @@ describe("Reporting", {
 
         cy.contains("footer", /total/i);
     });
+
+    it("can list transactions for a single project and gateway", function() {
+        cy.visit("/");
+
+        cy.contains("button", /projects/i).click().get("menu li:last-child").click();
+        cy.contains("button", /gateways/i).click().get("menu li:last-child").click();
+        cy.contains("button.action", "report").click();
+
+        cy.contains("header", "All projects").should("not.exist");
+        cy.contains("header", "All gateways").should("not.exist");
+
+        cy.get("table").should("exist");
+        cy.get("div.chart").should("not.exist");
+
+        cy.contains("footer", /total/i);
+    });
 });

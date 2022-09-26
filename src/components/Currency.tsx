53c3c908d79e45d842fd6fb44cb15eef88a996be
Mert Torun <mert@merttorun.com>
2022-09-26 16:14:35 +0300

    Define Props type for <Currency>

--- src/components/Currency.tsx
+++ src/components/Currency.tsx
@@ -1,4 +1,9 @@
-export default function Currency({ amount, code }) {
+type Props = {
+    amount: number
+    code: "USD" | "EUR" | "JPY"
+}
+
+export default function Currency({ amount, code }: Props) {
     const { format } = new Intl.NumberFormat("en-US", {
         style: "decimal",
         minimumFractionDigits: 0,

932f7e16238f9a39ee58c307d928f0f392cfffa7
Mert Torun <mert@merttorun.com>
2022-09-29 01:03:54 +0300

    Convert <DataSection> to TypeScript

--- src/components/DataSection.tsx
+++ src/components/DataSection.tsx
@@ -1,8 +1,19 @@
 import DataBlock from "./DataBlock";
 import "./DataSection.css";
 import { useState } from "react";
+import type { Column } from "./Table";
 
-export default function DataSection({ children: columns, data, header, blockHeader }) {
+interface Props<Data> {
+    children: ReturnType<typeof Column>[]
+    data: (Data[] & { key?: any, id?: any })[]
+    header?: React.ReactNode
+    blockHeader?: (data: Data[]) => React.ReactNode
+}
+
+
+export default function DataSection<Data>({ 
+    children: columns, data, header, blockHeader 
+}: Props<Data>) {
     const [openBlock, setOpenBlock] = useState(0);
     return (
         <section>

75a4d32af1e6fc3f2c2edfc49c6a3f9ad5ef536c
Mert Torun <mert@merttorun.com>
2022-09-29 15:27:15 +0300

    Generate request done event types from context keys

--- src/features/reporting/Reports.machine.ts
+++ src/features/reporting/Reports.machine.ts
@@ -39,10 +39,12 @@ interface Context {
 type Events =
   | { type: "userSubmittedReportRequest", filters: any }
   | { type: "userClickedRerty" } 
-  | { type: "done.invoke.reportRequest", data: { data: Context["report"] } }
-  | { type: "done.invoke.projectsRequest", data: { data: Context["projects"] } }
-  | { type: "done.invoke.gatewaysRequest", data: { data: Context["gateways"] } }
+  | RequestDoneEvent
   
+type RequestDoneEvent<Key extends keyof Context = keyof Context> = { 
+  type: `done.invoke.${Key}Request`, 
+  data: { data: Context[Key] } 
+}
   
 export default 
 /** @xstate-layout N4IgpgJg5mDOIC5QCUwAcD2AnALrAdADIYCGEAlgHZQAEYlO5jc+AClhgFZgDGeRpCtQDEEDJTD4qANwwBrSaky4CxMlVr1GzAuy69+aoVAQyMPEo3EBtAAwBdRKEywm5cU5AAPRAGYALLb4AKwAjABMwf4RAJwxobb+SQA0IACeiOEAbKH4-gDswQAcRcExvqG+MWH5AL61qUrYhoIadAxuLHrcfKqtImBYHFj4aAA2lgBm2AC2+E0qAurU7drkXRw9LcsmZhZWlHaOSCAubh4nPggAtOExWfi2WTH+RVlZVUXZRakZCKGlR7+YK+WzlHLhO6VeqNdDNPo7VadAgAcUsYAA7iQ0gjjKJxJIzAp8FB0VicagAI4AVzgOE8ZwOniuvnyQReWVsvl8wXyoX5-nCv0Q-hiQSStny4VF91C1XCMJAC22xiROnwaJwmOxuI0wkGw1GExw0ywc1JWvJsCptNg9JOjPclGZiGuoKC4SKopyXtKMXCtmCwoQUX8+DiOSyd2BAcSiuVupWWmR+Ik+Dt6PmcMWRjayZ0DIwriZl0Q+SK+XwWTZwWCWWCAficuDAWC4bKOT5EVCxSy-nj2f4AFEOjoaGNBJAs8ocDQ0PQ8WI00TFIOCCO1nBx5OINPmnOFxpTJRZPsnUdC8WnS6EC8ivhIaKClVOaEUuk-F8QiCkkUIvko3yOoGiVNd8A3ZFtzISBhGpWBBgAZWpAAjGYmC1CBlRtOlL3OZ1S1vXx72lflimBMV8iqYNQj5PIAjeEFAyeCoBxnAgAEFKDVNIaEmEhyDGSAaBwDAoIgWD4KwABhMZyB4BRMMGHA-mcIs8Jva5fzyCt7gAgEe0qfxqNse85QiL0xRiGs63qEDKAwCA4E8BMllVfN1l0TYDETKBcJLUArlue8iOrXx62iUU7khYNIgeQDAnuAo3hiUpWPhVy81HDy2C83pXMgPzrwIjt8CKHl-ByCJ6wbH4PwQT1ck7etIlsAMvmCNKc36TQso2fRekKi4AtdBt8BCyjwrff1-Wor0qyqBJeXiUo+SyTqVUyzdUTJHUMuoQb8OGm5wkrZ5FpOgMpWiSjgwqh4km5UJqyexIinWny1WyzVtRxfKIAOm8SrK2tWv8blW18YNgi5PI7lButygR969p6raNR2nEAYI64wireJAwuyUSPyFsXjyEGckDWJfAVECXNzJNevgB01P87xXQAsa3gmqIpqi6iIiraGilsOUeS9X9kYgscJ2g3cADlRKwNcsaO7twlK6tniauHar+JJKwbAoKOlHkailpmxKnZUD0oYxVfZ28KofJ4+zBlKeW5GKiPwUiUtsLl6z-QMLa2q3dxt2AAAsMAxQ7TlZoq1ble9pv9Tk+S5AIoZ9yLpT-Yp4k9UPINlxz-pZq8hsd+IwxInsvTKSUqLqis8gq4paySasXmRzjuN4-jBIgYTRLLh3AuBXxStZGm7neQIwSyFsqnbXl3ijOs7l8d6J9dApKzKyjIXuPsA-uai22BMrA3LQVqmrDrbKAA */

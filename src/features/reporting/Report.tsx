d7f2c823e88e1b673639eac831b6843702c43bb0
Mert Torun <mert@merttorun.com>
2022-10-03 02:33:35 +0300

    Convert <Report> to TypeScript
    Importing Cypress fixtures directly is impossible now without types. Used Zod parsing as if they were server-returned JSON. They actually are copied from server responses.

--- src/features/reporting/Report.tsx
+++ src/features/reporting/Report.tsx
@@ -1,9 +1,45 @@
 import { useMemo } from "react";
-import { DataSection, Date as Date_, Currency, Column, Summary } from "/src/components";
+import { DataSection, Date as Date_, Currency, Column, Summary } from "src/components";
 import PercentagePieChart from "./PercentagePieChart";
+import type { Project, Gateway, Payment } from "./Reports.schema"
 import "./Report.css";
+import * as React from "react";
 
-export default function Report(props) {
+interface Props {
+    projects: Project[]
+    gateways: Gateway[]
+    report: Payment[]
+}
+
+type Mode = "all" | "project" | "gateway" | "single"
+
+interface TransformedProps {
+    mode: Mode
+    names: { project: string, gateway: string }
+
+    total: number
+    totalTitle: string
+
+    chart: ((Project|Gateway) & ScopeExtension)[] | null
+
+    data: (Payment & PaymentExtension)[][]
+
+    renderHeader: (rows: PaymentExtension[]) => React.ReactNode
+}
+
+interface ScopeExtension {
+    total: number
+    blocks: (Payment & PaymentExtension)[]
+}
+
+interface PaymentExtension {
+    project: Project & ScopeExtension
+    gateway: Gateway & ScopeExtension
+}
+
+const PaymentColumn = Column<Payment & PaymentExtension>
+
+export default function Report(props: Props) {
     const {
         mode,
         data,
@@ -15,17 +51,17 @@ export default function Report(props) {
     } = useTransform(props);
 
     return (
-        <div id="report" className={mode}>
+        <div id="report" className={mode || ""}>
             <DataSection 
                 data={data}
                 header={`${names.project} | ${names.gateway}`}
                 blockHeader={renderHeader}>
-                <Column header="Date">{row => <Date_ date={row.created}/>}</Column>
-                {mode === "all" && <Column header="Gateway">{row => row.gateway.name}</Column>}
-                <Column header="Transaction ID">{row => row.paymentId}</Column>
-                <Column header="Amount">
+                <PaymentColumn header="Date">{row => <Date_ date={row.created}/>}</PaymentColumn>
+                {mode === "all" ? <PaymentColumn header="Gateway">{row => row.gateway.name}</PaymentColumn> : <></>}
+                <PaymentColumn header="Transaction ID">{row => row.paymentId}</PaymentColumn>
+                <PaymentColumn header="Amount">
                     {row => <Currency amount={row.amount} code="USD"/>}
-                </Column>
+                </PaymentColumn>
             </DataSection>
             { chart && <div className="chart"><PercentagePieChart data={chart}/></div> }
             <Summary>
@@ -35,30 +71,40 @@ export default function Report(props) {
     );
 }   
 
-function useTransform({ report, projects, gateways }) {
+function useTransform({ 
+    report: paymentArray, 
+    projects: projectArray, 
+    gateways: gatewayArray 
+}: Props): TransformedProps {
     return useMemo(() => {
-        projects = projects.reduce((map, project) =>
+        const projects: Map<string, Project & ScopeExtension> = projectArray.reduce((map, project) =>
             map.set(project.projectId, { ...project, total: 0, blocks: [] }),
         new Map());
-        gateways = gateways.reduce((map, gateway) =>
+        const gateways: Map<string, Gateway & ScopeExtension> = gatewayArray.reduce((map, gateway) =>
             map.set(gateway.gatewayId, { ...gateway, total: 0, blocks: [] }),
         new Map());
 
-        report.sort((a, b) => new Date(a.created) - new Date(b.created));
+        const report: (Payment)[] = [...paymentArray].sort((a, b) => a.created.valueOf() - b.created.valueOf());
 
         let total = 0;
+        const noProject = { projectId: "", name: "No project", total: 0, blocks: [] };
+        const noGateway = { gatewayId: "", name: "No gateway", total: 0, blocks: [] };
 
-        for (const block of report) {
-            block.project = projects.get(block.projectId);
-            block.gateway = gateways.get(block.gatewayId);
+        for (const rawBlock of report) {
+            const block = rawBlock as Payment & PaymentExtension
+            block.project = projects.get(block.projectId) || noProject;
+            block.gateway = gateways.get(block.gatewayId) || noGateway;
 
-            block.project.blocks.push(block);
-            block.gateway.blocks.push(block);
+            block.project?.blocks.push(block);
+            block.gateway?.blocks.push(block);
 
             total += block.amount;
             block.gateway.total += block.amount;
             block.project.total += block.amount;
         }
+
+        projects.set("", noProject);
+        gateways.set("", noGateway);
 
         for (const project of projects.values())
             if (!project.blocks.length) 
@@ -67,11 +113,12 @@ function useTransform({ report, projects, gateways }) {
             if (!gateway.blocks.length)
                 gateways.delete(gateway.gatewayId);
 
-        const mode =
+        const mode: Mode =
             projects.size > 1 && gateways.size > 1 && "all" ||
             projects.size === 1 && gateways.size > 1 && "project" ||
             projects.size > 1 && gateways.size === 1 && "gateway" ||
-            projects.size === 1 && gateways.size === 1 && "single";
+            projects.size === 1 && gateways.size === 1 && "single" ||
+            "all";
 
         const data = {
             all: [...projects.values()].map(project => project.blocks),
@@ -91,9 +138,9 @@ function useTransform({ report, projects, gateways }) {
         }[mode];
 
         const renderHeader = {
-            all: rows => rows.values().next().value.project.name,
-            project: rows => rows.values().next().value.gateway.name,
-            gateway: rows => rows.values().next().value.project.name,
+            all: (rows: PaymentExtension[]) => rows.values().next().value.project.name,
+            project: (rows: PaymentExtension[]) => rows.values().next().value.gateway.name,
+            gateway: (rows: PaymentExtension[]) => rows.values().next().value.project.name,
             single: () => null
         }[mode];
 
@@ -113,5 +160,5 @@ function useTransform({ report, projects, gateways }) {
 
         return { mode, data, names, renderHeader, total, totalTitle, chart };
 
-    }, [report, projects, gateways]);
+    }, [paymentArray, projectArray, gatewayArray]);
 }
